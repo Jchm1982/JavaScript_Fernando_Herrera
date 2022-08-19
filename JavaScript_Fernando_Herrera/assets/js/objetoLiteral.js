c48ce40838eb29eb1987d1118d07227fcfa81048
Juan C <j.hernandez@softhink.com.mx>
2022-08-19 18:30:21 -0500

    ofice19082022_1830

--- JavaScript_Fernando_Herrera/assets/js/objetoLiteral.js
+++ JavaScript_Fernando_Herrera/assets/js/objetoLiteral.js
@@ -30,3 +30,40 @@ console.log('Ultimo traje',personaje.trajes[personaje.trajes.length - 1]);
 const x = 'vivo';
 console.log('Vivo ?',personaje[x]);
 console.log('Ultima Pelicula',personaje['ultima pelicula']);
+
+
+//############## MAS DETALLES ##############
+//BORRAR UNA PROPIEDAD
+//personaje.edad = null;
+delete personaje.edad;
+
+//crear una nueva propiedad en el objeto
+personaje.casado = true;
+
+
+//TRABAJAR NUESTRO OBJETO COMO UN ARREGLO
+const entriePares = Object.entries( personaje );
+console.log(entriePares);
+
+//Para el bloqueo de las propiedades o las modificaciones
+/*Nota:Congela las asignaciones a c/u de las propeidades, pero no a los objetos
+que estan dentro de ese objeto, el ejmplo se puede cambiar la cordenadas.lat
+*/
+
+Object.freeze(personaje);
+
+personaje.dinero = 1000000;
+personaje.casado = false;
+console.log(personaje);
+
+//Necesito un listado de todas las propiedades que tiene ese objeto
+const propiedades = Object.getOwnPropertyNames( personaje);
+//Necesito un listado de todas las propiedades que tiene ese objeto, y tambien los valores
+const valores = Object.values( personaje);
+console.log({ propiedades, valores });
+
+/*
+URL DE APOYO
+
+https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object
+*/
