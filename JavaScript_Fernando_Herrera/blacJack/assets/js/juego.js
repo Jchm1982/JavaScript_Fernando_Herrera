4136088c082125cd215e1b2446282ade3d522251
Juan C <j.hernandez@softhink.com.mx>
2022-09-02 23:18:37 -0500

    casa_02092022_2318

--- JavaScript_Fernando_Herrera/blacJack/assets/js/juego.js
+++ JavaScript_Fernando_Herrera/blacJack/assets/js/juego.js
@@ -9,8 +9,20 @@ let deck = [];
 const tipos = ['C','D','H','S'];
 const especiales = ['A','J','Q','K'];
 
-//Esta funcion crea una nueva baraja
+let puntosJugador = 0,
+    puntosComputadora = 0;
 
+//Referencias de HTML
+const btnPedir = document.querySelector("#btnPedir");
+const btnDetener = document.querySelector("#btnDetener");
+
+const divCartasJugador = document.querySelector("#jugador-cartas");
+const divCartasComputadora = document.querySelector("#computadora-cartas");
+
+const puntosHTML = document.querySelectorAll('small');
+
+
+//Esta funcion crea una nueva baraja
 const crearDeck = () => {
    for( let i = 2; i<= 10; i++){
       for(let tipo of tipos){
@@ -37,9 +49,10 @@ const pedirCarta = () => {
       throw 'No hay cartas en el deck';
    }
    const carta = deck.pop();
-
+   /*
    console.log(deck);
    console.log(carta);//La carta debe ser de la baraja
+   */ 
    return carta;
 }
 /*
@@ -76,5 +89,55 @@ const valorCarta = ( carta ) => {
           : valor * 1;
 }
 
-const valor = valorCarta( pedirCarta() );
-console.log ({valor});
+//Este es el turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+   do {
+       const carta = pedirCarta();
+      puntosComputadora = puntosComputadora + valorCarta( carta );
+      puntosHTML[1].innerText = puntosComputadora;
+      //<img class="carta" src="assets/cartas/10H.png"></img>
+      const imgCarta = document.createElement('img');
+      imgCarta.src = `assets/cartas/${ carta }.png`;
+      imgCarta.classList.add('carta'); 
+      divCartasComputadora.append(imgCarta);
+      if(puntosMinimos > 21){
+         break;
+      }
+      
+   } while ( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+}
+
+//const valor = valorCarta( pedirCarta() );
+//console.log ({valor});
+
+//Eventos
+btnPedir.addEventListener('click',() => {
+      const carta = pedirCarta();
+      puntosJugador = puntosJugador + valorCarta( carta );
+      puntosHTML[0].innerText = puntosJugador;
+      //<img class="carta" src="assets/cartas/10C.png" alt="">
+      const imgCarta = document.createElement('img');
+      imgCarta.src = `assets/cartas/${ carta }.png`;
+      imgCarta.classList.add('carta'); 
+      divCartasJugador.append(imgCarta);
+
+      //Controlar los puntos
+      if( puntosJugador > 21){
+         console.warn('Ya perdiste')
+         btnPedir.disabled = true;
+         btnDetener.disabled = true;
+         turnoComputadora(puntosJugador);
+      }else if(puntosJugador === 21){
+         console.warn('21, Genial');
+         btnPedir.disabled = true;
+         btnDetener.disabled = true;
+         turnoComputadora(puntosJugador);
+      }
+});
+
+
+btnDetener.addEventListener('click',() => {
+      btnPedir.disabled = true;
+      btnDetener.disabled = true;
+      turnoComputadora(puntosJugador);
+});
