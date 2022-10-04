591fcc18606e3638b3674d84ec58363aa067edb7
Juan C <j.hernandez@softhink.com.mx>
2022-10-04 09:16:24 -0500

    oficina_03102022_1741

---
+++ JavaScript_classes/assets/classes/problema.js
@@ -0,0 +1,14 @@
+const fer ={
+    nombre: 'Fernando',
+    edad: 40
+    imprimir (){
+        console.log(`Nombre: ${ this.nombre}`)
+    }
+}
+
+const pedro ={
+    nombre: 'Pedro',
+    edad: 39
+}
+
+fer.imprimir();
