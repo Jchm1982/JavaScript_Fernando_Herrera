36010d59aab15e749f87958b445909f9f68f9a10
Juan C <j.hernandez@softhink.com.mx>
2022-10-05 00:56:21 -0500

    casa_05102022_0056

--- JavaScript_classes/assets/classes/clases.js
+++ JavaScript_classes/assets/classes/clases.js
@@ -1,8 +1,18 @@
 class Persona {
+    static _conteo = 0;
+    static get getConteo(){
+        return Persona._conteo + ' instancias';
+    }
+
+    static mensaje(){
+        console.log(this.nombre); //Undefined
+        console.log('Hola a todos, soy un metodo statico');
+    }
     
     nombre = '';
     codigo = '';
-    frase = ''; 
+    frase = '';
+    comida = '';
 
     constructor(nombre = 'Sin Nombre',codigo='Sin codigo',frase= 'Sin frase'){
         //El constructor es el unico metodo, que retorna una instancia de 
@@ -11,6 +21,17 @@ class Persona {
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+        Persona._conteo++;
+    }
+
+    //Para establecer algun valor
+    set setComidaFavorita( comida ){
+        this.comida = comida.toUpperCase();
+    }
+
+    //Recupera una propiedad construida
+    get getComidafavorita (){
+        return `La comida favorita ${ this.nombre } es ${ this.comida}`;
     }
 
     quienSoy(){
@@ -24,9 +45,22 @@ class Persona {
 
 const spiderman = new Persona('peter Parker','Spiderman','Tu amigo el hombre Araña');
 const ironman = new Persona('Tony Stark','Ironman','Soy un genio de la tecnologia');
-console.log(spiderman);
-console.log(ironman);
+//console.log(spiderman);
+//console.log(ironman);
 
 
 spiderman.miFrase();
-ironman.miFrase();
+//ironman.miFrase();
+spiderman.setComidaFavorita = 'Postre de la tia Mey';
+//spiderman.nemesis = 'Venom';
+//console.log(spiderman.getComidafavorita);
+//console.log(spiderman);
+//Persona._conteo = 2;
+console.log(`Conteo Statico`, Persona._conteo);
+console.log(Persona.getConteo);
+Persona.mensaje();
+
+//Se puede trabajar con metodos estaticos
+Persona.propiedadExterna = 'Hello World';
+console.log(Persona.propiedadExterna);
+console.log(Persona);
